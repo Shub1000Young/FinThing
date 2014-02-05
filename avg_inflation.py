fd518a96ace2653b2eb22de6996b621e5c20f1be
Trullzer <traustim90@gmail.com>
2014-02-05 15:17:24 +0000

    avg inflation fall
    bætti við falli sem reiknar út meðaltal verðbólgu.

---
+++ avg_inflation.py
@@ -0,0 +1,31 @@
+
+import csv
+
+def avg_inflation(start,end):
+    #count heldur utan um fjolda manuda
+    #verd_b heldur utan um raunverdbolgu fyrir hvern manud.
+    #medalverdbolga heldur utan um medalverbolgu a timabili.
+    count = 0
+    verd_b = []
+    medalverdbolga = 0
+    with open('verdbolga.txt','rb') as verdbolga_skjal:
+        verdbolga_skjal = csv.reader(verdbolga_skjal, delimiter='\t')
+
+        for row in verdbolga_skjal:
+            if (start==row[0]):# tegar start day byrjar
+                verd_b.append(row[1])
+                count=count+1# baetir verdbolgu vid shit[]
+                for row in verdbolga_skjal:
+                    verd_b.append(row[1]) #baetir verdbolgu vid shit[]
+                    count=count+1
+                    if (end==row[0]):
+                        break      # heattir tegar naer end day
+                verd_b = map(float,verd_b) #shit var listi med string en convertum yfir i float
+                medalverdbolga = (sum(verd_b)/count) # medalverdbolga fra start til end day
+                return medalverdbolga
+
+
+x=avg_inflation("2007-12","2019-01")
+print x
+#x=avg_inflation("1990-05","2009-01")
+#print x
