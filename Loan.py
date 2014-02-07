05ff29cd559ab5e008525226cd71df63564af002
ippon87 <sob15@hi.is>
2014-02-07 15:52:46 +0000

    Núna skilar fallið réttum heildar hagnaði
    Þarf seinna að gefa niður einhverskonar mánaðarlegri greiðslu skiptingu
    fyrir línurit

--- Loan.py
+++ Loan.py
@@ -22,36 +22,48 @@ class Loan:
         i_rate += avg_inflation("1990-1","2013-1") #Fasti sem verður fall seinna
 
     self.r = self.i_rate / self.comp_freq #vaxtaprósenta fyrir vaxtatímabil
-    self.tot_loan = self.principle*(1.0 + self.r)**(self.comp_freq * (term/12)) # heildar upphæð sem á eftir að borga
+    self.tot_loan = (((self.r)*self.principle)/(1-(1+(self.r))**-self.term))*self.term # heildar upphæð sem á eftir að borga
     self.m_paymnt = self.tot_loan / self.term # Mánaðarleg greiðsla
     self.tot_int = self.tot_loan - self.principle # Hversu stór hluti heildarlánsins séu vextir
     self.m_int = (((1+self.i_rate/self.comp_freq)**(self.comp_freq/12))-1)*self.principle #hlutfall vaxta næstu greiðslu
 
-#enduukvæmt fall sem keyrir í gegnum mánuðina með yfirgreiðslu og 	endurreiknar vexti hvert sinn
-def recalc(loan, t_elapsed, overpay_amt):
-	if t_elapsed <= 0: # tíminn búinn, skila
-		return [loan.tot_loan, loan.tot_int, loan.principle]
-	else: #gera nýtt lán (Python er call by value) og greiða það niður um einn mánuð
-		newloan = Loan(loan.name, loan.principle, loan.i_rate, loan.term, loan.comp_freq, loan.indexed)
-		principlePayment = loan.m_paymnt - loan.m_int + overpay_amt
-		newloan.principle -= principlePayment
-		newloan.term -= 1
-		return recalc(newloan, t_elapsed-1, overpay_amt)
+#enduukvæmt fall sem keyrir í gegnum mánuðina með yfirgreiðslu og endurreiknar vexti hvert sinn
+# def recalc(loan, t_elapsed, overpay_amt):
+	# if t_elapsed <= 0: # tíminn búinn, skila
+		# return [loan.tot_loan, loan.tot_int, loan.principle]
+	# else: #gera nýtt lán (Python er call by value) og greiða það niður um einn mánuð
+		# newloan = Loan(loan.name, loan.principle, (loan.i_rate*100.0), loan.term, loan.comp_freq, loan.indexed)
+		# principlePayment = loan.m_paymnt - loan.m_int + overpay_amt
+		# newloan.principle -= principlePayment
+		# newloan.term -= 1
+		# return recalc(newloan, t_elapsed-1, overpay_amt)
 
+#lúppa sem fer í gegnum lánið, greiðir niður og reiknar vexti
+def recalc(loan, t_elapsed, overpay_amt):
+	tot_intP = 0 # heildargreiddir vextir
+	loanA = Loan("loan1", loan.principle, loan.i_rate*100.0, loan.term, loan.comp_freq, loan.indexed)#Nýtt lán byggt á láni
+	for i in range(t_elapsed):
+		loanB = Loan("loanB", loanA.principle, loanA.i_rate*100.0, loanA.term, loanA.comp_freq, loanA.indexed)#Nýtt lán byggt á loanA
+		principlePayment = loanA.m_paymnt - loanA.m_int + overpay_amt #Greiðsla af höfuðstól
+		tot_intP += loanA.m_int #heildar vextir greiddir
+		loanB.principle -= principlePayment #greiða niður höfuðstól
+		loanB.term -= 1 # minka tímabil
+		loanA = Loan("loanA", loanB.principle, loanB.i_rate*100.0, loanB.term, loanB.comp_freq, loanB.indexed) #LoanA er nýja lánið
+	return (loan.tot_int - loanA.tot_int - tot_intP) #skila heildar vaxtasparanaði
+	
+	
 # Finna besta hagnað fyrir pening og tíma
 def evaluate(loans, months, amount):
 	totalSpent = months * amount
 	newtotal = []
 	maxprof = 0
 	index = 0
-	interestprof = 0
 	for i in range(len(loans)):
 		newtotal= recalc(loans[i], months, amount)
-		if loans[i].tot_loan - newtotal[0] > maxprof:
-			maxprof = loans[i].tot_loan - newtotal[0]
+		if newtotal > maxprof:
+			maxprof = newtotal
 			index = i
-			interestprof = loans[i].tot_int - newtotal[1]
 
     #return {"nafn" : loans[index].name, "sparnadur" : maxprof - totalSpent, "vextir" : interestprof}
 
-	return [loans[index].name, maxprof - totalSpent, interestprof]
+	return [loans[index].name, maxprof, interestprof]
