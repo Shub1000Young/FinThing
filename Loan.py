4e0e57e8efb17212286c5f4c0d4b459fb5fc7abf
ippon87 <sob15@hi.is>
2014-02-19 17:09:22 +0000

    overview fall komið í loan.py

--- Loan.py
+++ Loan.py
@@ -30,6 +30,7 @@ class Loan:
 
 
 #lúppa sem fer í gegnum lánið, greiðir niður og reiknar vexti
+
 def recalc(loan, t_elapsed, overpay_amt):
 	tot_intP = 0 # heildargreiddir vextir
 	loanA = Loan("loan1", loan.principle, loan.i_rate*100.0, loan.term, loan.comp_freq, loan.indexed)#Nýtt lán byggt á láni
@@ -77,3 +78,32 @@ def evaluate(loans, months, amount):
 	if len(intermedLoans) != 0:
 		return [intermedLoans, loans[index].name, maxprof]
 	return [loans[index].name, maxprof, maxprof]
+
+#þetta fall skilar tveimur listum af mánaðarlegum eftirstöðvum fyrir línurit
+def overview(loan, t_elapsed, overpay_amt):
+	overpayTime = t_elapsed
+	original = [] # eftistöðvar með engum yfirgreiðslum
+	newlist = [] # eftirstöðvar með yfirgreiðslum
+	loanA = Loan("loan1", loan.principle, loan.i_rate*100.0, loan.term, loan.comp_freq, loan.indexed)#Nýtt lán byggt á láni
+	for i in range(loan.term-1): #borgar niður lán og bætir eftirstöðvum í lista
+		loanB = Loan("loanB", loanA.principle, loanA.i_rate*100.0, loanA.term, loanA.comp_freq, loanA.indexed)#Nýtt lán byggt á loanA
+		original.append(int(loanB.principle))
+		principlePayment = loanA.m_paymnt - loanA.m_int
+		loanB.principle -= principlePayment #greiða niður höfuðstól
+		loanB.term -= 1 # minka tímabil
+		loanA = Loan("loanA", loanB.principle, loanB.i_rate*100.0, loanB.term, loanB.comp_freq, loanB.indexed) #LoanA er nýja lánið
+	original.append(0)
+	loanA = Loan("loan1", loan.principle, loan.i_rate*100.0, loan.term, loan.comp_freq, loan.indexed)#Nýtt lán byggt á láni
+	for i in range(loan.term-1):#listi af eftirstöðvum með yfirborgunum
+		loanB = Loan("loanB", loanA.principle, loanA.i_rate*100.0, loanA.term, loanA.comp_freq, loanA.indexed)#Nýtt lán byggt á loanA
+		newlist.append(int(loanB.principle))
+		if overpayTime > 0:
+			principlePayment = loanA.m_paymnt - loanA.m_int + overpay_amt
+			overpayTime -=1
+		else:	
+			principlePayment = loanA.m_paymnt - loanA.m_int
+		loanB.principle -= principlePayment #greiða niður höfuðstól
+		loanB.term -= 1 # minka tímabil
+		loanA = Loan("loanA", loanB.principle, loanB.i_rate*100.0, loanB.term, loanB.comp_freq, loanB.indexed) #LoanA er nýja lánið
+	newlist.append(0)
+	return [original, newlist]
