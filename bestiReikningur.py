ee5cb5d0bf61988d3e3bd1699850dd3e9266716a
Trullzer <traustim90@gmail.com>
2014-02-19 11:54:16 +0000

    finnur bestar sparireikning
    sparireikningar.

--- bestiReikningur.py
+++ bestiReikningur.py
@@ -1,22 +1,25 @@
-
+﻿
 #coding=UTF-8
 import savings
 def vaxtatrep(amount, term):
-    intrest=3.95  ############################################ this is wrong!
-    trepreikn=savings.savings("vaxtaþrep",term,intrest,0)   #
-    trepreikn.pay(amount)                                   #
+    intrest=3.95  ############################################ virkar ekki alveg
+    trepreikn=savings.savings("vaxtaþrep",term,intrest,0)   #  reiknar alltaf med 4.25 % voxtum
+                                                            # nema nu tegar se heill hellingur inna reikn..
     if (5000000 <= trepreikn.balance < 20000000):           #
         intrest=4.25                                        #
     elif (20000000 <= trepreikn.balance < 75000000):        #
         intrest=4.55                                        #
     elif (trepreikn.balance >=75000000):                    #
-        intrest=4.85                                        #
+        intrest=4.85
+    trepreikn.pay(amount)                                    #
                                                             #
     return round(trepreikn.balance,0)                       #
-####################################################################### wrong
+####################################################################### 
 
 def fastvaxtareikningur(amount, term): # aetti ad vera rett
     intrest=0
+    if (term < 3):
+        intrest=3.92
     if (3 <= term < 6):
         intrest=4.91
     if (6 <= term < 12):
@@ -28,7 +31,7 @@ def fastvaxtareikningur(amount, term): # aetti ad vera rett
     fastvaxta.pay(amount)
     return round(fastvaxta.balance,0)
 
-def vaxtasproti(amount, term):
+def vaxtasproti(amount, term):  #aetti ad vera rett
     intrest = 3.6
     sprotireikningur=savings.savings("vaxtasproti",term, intrest,0)
     sprotireikningur.pay(amount)
@@ -40,9 +43,9 @@ def calculatesavings(amount, term):
     list.append(fastvaxtareikningur(amount, term))
     list.append(vaxtasproti(amount, term))
 
-    x = max(list)
-    if (list[0]==x):
-        return (list[0],"Vaxtaþrep")
+    x = max(list)  # finnum maxid i listanum
+    if (list[0]==x):                       #berum saman vid stokin 3 i listanum
+        return (list[0],"Vaxtaþrep")        # og finnum nafn reiknings
     elif (list[1]==x):
         return (list[1],"Fastvaxtareikningur")
     elif (list[2]==x):
